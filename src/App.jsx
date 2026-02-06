4d549850b1ee95179286831ec3ad2ad0b7d34c25
DXiaodai <dyb25@mails.tsinghua.edu.cn>
2026-02-06 17:05:04 +0800

    我们的第一个像样的场景

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
+import * as THREE from 'three'
 
 function App() {
   return(
-    <Canvas 
-    camera={{
-      position: [3,3,3]
-    }}>
+    <Canvas camera={{position: [3,3,3]}}>
       <OrbitControls />
-      <mesh>
+      <mesh position-x={-0.9}>
         <boxGeometry />
-        <meshNormalMaterial />
+        <meshStandardMaterial color={0x00ff00} side={THREE.FrontSide} />
       </mesh>
+       <mesh position-x={-0.9} position-z={-2}>
+        <boxGeometry />
+        <meshStandardMaterial color={0x00ff00} side={THREE.BackSide} />
+      </mesh>
+      <mesh position-x={0.6} position-z={1}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial color={0xff0000} />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[1, 1, 1]} intensity={0.5} />
+      <directionalLight position={[0, 0, 2]} intensity={0.5} />
     </Canvas>
   )
 }
