10b74a3b6019f48b08ef1c4d847790e239cea4dd
DXiaodai <dyb25@mails.tsinghua.edu.cn>
2026-02-15 19:38:40 +0800

    更新Project数据模型

--- src/hooks/useProject.jsx
+++ src/hooks/useProject.jsx
@@ -8,33 +8,35 @@ export function ProjectProvider({ children }) {
   const [projectData, setProjectData] = useState({
     'Hajimi-123456': {
       name: 'Vigorous-Test-Project',
-      id: 'Hajimi-123456',
-      description: 'Oiiaioiiiiai',
       user: 'Hajimi',
       created_at: '创建时间不明',
       edited_at: '修改时间也不明',
-      status: '还差亿点',
-      pieces: [],
-      shape: '基本单位的形状',
-      size: '基本单位的大小。',
-      tags: ['type1','type2'],
+      id: 'Hajimi-123456',
+      description: 'Oiiaioiiiiai',
+      status: 'editable',
+      feature:{
+        shape: 'square',
+        size: 10,
+      },
+      project_tags: ['type1','type2'],
     },
     'Hajimi-456789': {
-      name: 'Vigorous-Test-Project-2',
+      name: 'Vigorous-Test-Project',
+      user: 'Hajimi',
+      created_at: '创建时间不明',
+      edited_at: '修改时间也不明',
       id: 'Hajimi-456789',
       description: '第二个测试项目',
-      user: 'Hajimi',
-      created_at: '20260215',
-      edited_at: '202602151',
-      status: '进行中',
-      pieces: [],
-      shape: '基本单位的形状',
-      size: '基本单位的大小。',
-      tags: ['type2','type3','type4'],
-    }
+      status: 'archived',
+      feature:{
+        shape: 'square',
+        size: 10,
+      },
+      project_tags: ['type3','type4'],
+    },
   });
 
-  //这里马上要写数据逻辑，从后端获取项目数据，向后端同步数据。
+  //这里马上要写逻辑和方法，从后端获取项目数据，向后端同步数据。
 
 
   //这里是Context的结尾和Hook，后面是固定的。
