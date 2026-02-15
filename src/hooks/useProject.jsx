9eee2633123154bc7c8f8a2a6bc7e87bd333d29b
DXiaodai <dyb25@mails.tsinghua.edu.cn>
2026-02-15 18:55:47 +0800

    Context完善1

--- src/hooks/useProject.jsx
+++ src/hooks/useProject.jsx
@@ -3,6 +3,8 @@ import { useState, useContext, createContext } from 'react';
 const ProjectContext = createContext(null);
 
 export function ProjectProvider({ children }) {
+
+  // 初始化项目数据。现在是模拟数据。
   const [projectData, setProjectData] = useState({
     'Hajimi-123456': {
       name: 'Vigorous-Test-Project',
@@ -12,11 +14,10 @@ export function ProjectProvider({ children }) {
       created_at: '创建时间不明',
       edited_at: '修改时间也不明',
       status: '还差亿点',
-      pieces: '下辖的棋子id集合。',
+      pieces: [],
       shape: '基本单位的形状',
       size: '基本单位的大小。',
-      tags: '项目标签1',
-      chess_tags: '项目下辖棋子的标签',
+      tags: ['type1','type2'],
     },
     'Hajimi-456789': {
       name: 'Vigorous-Test-Project-2',
@@ -26,14 +27,17 @@ export function ProjectProvider({ children }) {
       created_at: '20260215',
       edited_at: '202602151',
       status: '进行中',
-      pieces: '下辖的棋子id集合。',
+      pieces: [],
       shape: '基本单位的形状',
       size: '基本单位的大小。',
-      tags: '项目标签2',
-      chess_tags: '项目下辖棋子的标签',
+      tags: ['type2','type3','type4'],
     }
   });
 
+  //这里马上要写数据逻辑，从后端获取项目数据，向后端同步数据。
+
+
+  //这里是Context的结尾和Hook，后面是固定的。
   return (
     <ProjectContext.Provider value={{ projectData, setProjectData }}>
       {children}
